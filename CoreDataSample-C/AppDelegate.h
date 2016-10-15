cfd2b3e5e46674b8cffea8230fe94184151afe53
GengRui <gengrui@gengruis-mac-mini.local>
2016-10-15 19:40:24 -0400

    Initial Commit

---
+++ CoreDataSample-C/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataSample-C
+//
+//  Created by GengRui on 2016-10-15.
+//  Copyright © 2016 GengRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CoreDataSample-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataSample-C
+//
+//  Created by GengRui on 2016-10-15.
+//  Copyright © 2016 GengRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
